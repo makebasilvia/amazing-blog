bebff8db43aae8296340ca63adaca3c54282c6a6
mewmewproopmew <576590@student.belgiumcampus.ac.za>
2022-08-09 14:13:14 +0200

    Change sqlite to postgres.

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ from flask import abort
 from dotenv import load_dotenv
 import os
 
-SECRET_KEY=os.getenv("SECRET KEY")
-DB_URI=os.getenv("DB_URI")
+# SECRET_KEY=os.getenv("SECRET KEY")
+# DB_URI=os.getenv("DB_URI")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] =os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
@@ -27,7 +27,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
